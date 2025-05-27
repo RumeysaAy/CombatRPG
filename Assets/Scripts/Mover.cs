89a0c896d54b606c681aa0b6f5ab98db107a1a37
Rumeysa AY <rumeeysaay@gmail.com>
2025-05-27 14:54:21 +0300

    Match Animation To Movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -13,6 +13,8 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -25,6 +27,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
 
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
