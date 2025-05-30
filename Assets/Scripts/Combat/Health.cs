727b0882dad78a26d40fcfbf7b240e41bf98f95b
Rumeysa AY <rumeeysaay@gmail.com>
2025-05-30 16:49:25 +0300

    Trigger Death Animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,13 +6,24 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] private float health = 100f;
+        [SerializeField] private float healthPoints = 100f;
+        bool isDeath = false;
 
-        //
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+            print(healthPoints);
+        }
+
+        private void Die()
+        {
+            if (isDeath) return;
+            GetComponent<Animator>().SetTrigger("die");
+            isDeath = true;
         }
     }
 }
